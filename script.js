c454b1e3a5b9ae70cee402ac0a87bb224ccce319
ggmxmx <80310226+ggmxmx@users.noreply.github.com>
2025-05-12 16:57:39 +0300

    malek

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-fetch("https://myfakeapi.com/api/cars/")
+fetch("https://rickandmortyapi.com/api/character")
         .then(response => response.json())
-        .then(cars => {
+        .then(data => {
           const container = document.getElementById('content');
-          cars.forEach(car => {
+          data.results.forEach(character => {
             const itemDiv = document.createElement('div');
             itemDiv.className = 'item'; // Optional: Add CSS class
             itemDiv.innerHTML = `
-              <h3>${car.car}</h3>
-                <p>${car.model}</p>
-                <p>${car.price}</p>
-                <p>${car.car_color}</p>
-            
+               <h3>${character.name}</h3>
+        <p>Status: ${character.status}</p>
+        <p>Species: ${character.species}</p>
+        <p>Gender: ${character.gender}</p>
+        <img src="${character.image}" alt="${character.name}" />
             `;
             container.appendChild(itemDiv);
 
-            console.log(car);
+            console.log(character);
             
           });
         })
