606ed34eb70ded82ef78d00a7b8b7dec7b588340
ggmxmx <80310226+ggmxmx@users.noreply.github.com>
2025-05-17 08:58:20 +0300

    add is owned button

--- script.js
+++ script.js
@@ -1,33 +1,59 @@
 fetch("https://rickandmortyapi.com/api/character")
-        .then(response => response.json())
-        .then(data => {
-          const container = document.getElementById('content');
-          data.results.forEach(character => {
-            const itemDiv = document.createElement('div');
-            itemDiv.className = 'item'; // Optional: Add CSS class
-            itemDiv.innerHTML = `
-               <h3>${character.name}</h3>
-        <p>Status:<br> ${character.status}</p>
-        <p>Species:<br> ${character.species}</p>
-        <p>Gender:<br> ${character.gender}</p>
-        <img src="${character.image}" alt="${character.name}" />
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('content');
+    data.results.forEach(character => {
+      const itemDiv = document.createElement('div');
+      const ownButton = document.createElement('button');
+      ownButton.innerText = "Own";
+      itemDiv.className = 'item'; // Optional: Add CSS class
+      container.appendChild(itemDiv);
+      itemDiv.innerHTML = `
+              <h3>${character.name}</h3>
+              <p>Status:<br> ${character.status}</p>
+              <p>Species:<br> ${character.species}</p>
+              <p>Gender:<br> ${character.gender}</p>
+              <img src="${character.image}" alt="${character.name}" />
             `;
-            container.appendChild(itemDiv);
-
-            console.log("hello")
-            console.log(character);
-            
-            if (character.species === "Human") {
-                itemDiv.className = 'locked';
-            }
-          });
-
-          
-        })
-        .catch((error) => {
-            console.log("Error fetching categories:", error);
-        });
+            itemDiv.appendChild(ownButton);
             
 
+            ownButton.addEventListener('click', (isOwned) => {
+              isOwned = false
+              if (isOwned) {
+                isOwned = false;
+                itemDiv.classList.remove('owned');
+                ownButton.innerText = "Own";
+                
+                console.log(isOwned);
+                
+              } else {
+                isOwned = true;
+                itemDiv.classList.add('owned');
+                ownButton.innerText = "Unown";
+               
+                console.log(isOwned);
+              }
+            });
 
-    
+
+     
+
+      console.log("hello")
+      console.log(character);
+
+      if (character.species === "Human") {
+        itemDiv.className = 'locked';
+        itemDiv.removeChild(ownButton);
+      }
+    });
+
+
+  })
+  .catch((error) => {
+    console.log("Error fetching categories:", error);
+  });
+
+
+
+
